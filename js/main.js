8e1b8d922e99e30533d5636ab7194677cdf0fd88
MayerDigital <christian.mayer@mayer-bauzeichnungen.de>
2026-04-02 13:23:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -195,7 +195,7 @@ async function handleStartWork() {
 
   try {
     if (me?.working) {
-     await startWork(participantId, state.currentRoom); // 🔓 FREIGEBEN
+    await startWork(null, state.currentRoom); // 🔓 FREIGEBEN
       setStatus(dom.statusBox, "Arbeit beendet");
     } else {
       await startWork(participantId, state.currentRoom);
