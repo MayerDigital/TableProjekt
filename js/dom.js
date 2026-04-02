bdfb8f99ab3cbb04794bdc018aba92a1c25ce13a
MayerDigital <christian.mayer@mayer-bauzeichnungen.de>
2026-04-02 08:00:02 +0200

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,14 +1,37 @@
-export function getEl(id) {
-  return document.getElementById(id);
+import { SELECTORS } from "./config.js";
+
+function getElement(selector) {
+  const element = document.querySelector(selector);
+
+  if (!element) {
+    console.warn(`Element nicht gefunden: ${selector}`);
+  }
+
+  return element;
 }
 
-export const getParticipantsBox = () => getEl("participantsBox");
-export const getWorkerBox = () => getEl("workerBox");
-export const getOwnerBox = () => getEl("ownerBox");
-export const getChatMessagesBox = () => getEl("chatMessages");
-export const getChatInput = () => getEl("chatInput");
-export const getFilesArea = () => getEl("filesArea");
-export const getImagesArea = () => getEl("imagesArea");
-export const getTextsArea = () => getEl("textsArea");
-export const getShareScreenBtn = () => getEl("shareScreenBtn");
-export const getExpertPanel = () => getEl("expertPanel");
+export const dom = {
+  nameInput: getElement(SELECTORS.nameInput),
+  roomInput: getElement(SELECTORS.roomInput),
+  createRoomBtn: getElement(SELECTORS.createRoomBtn),
+  joinRoomBtn: getElement(SELECTORS.joinRoomBtn),
+  statusBox: getElement(SELECTORS.statusBox),
+
+  visualStatus: getElement(SELECTORS.visualStatus),
+  speakerStatus: getElement(SELECTORS.speakerStatus),
+  micStatus: getElement(SELECTORS.micStatus),
+
+  toggleVisualBtn: getElement(SELECTORS.toggleVisualBtn),
+  toggleSpeakerBtn: getElement(SELECTORS.toggleSpeakerBtn),
+  toggleMicBtn: getElement(SELECTORS.toggleMicBtn),
+
+  currentRoomLabel: getElement(SELECTORS.currentRoomLabel),
+  workStatusLabel: getElement(SELECTORS.workStatusLabel),
+  participantCountBadge: getElement(SELECTORS.participantCountBadge),
+  participantsList: getElement(SELECTORS.participantsList),
+
+  chatInput: getElement(SELECTORS.chatInput),
+  sendChatBtn: getElement(SELECTORS.sendChatBtn),
+  chatMessages: getElement(SELECTORS.chatMessages),
+  tableStage: getElement(SELECTORS.tableStage),
+};
