9c5752b249cdf93c3ed2b0e799ec4e7ed954ee9c
MayerDigital <christian.mayer@mayer-bauzeichnungen.de>
2026-04-02 09:51:39 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const APP_NAME = "TableProjekt™";
 export const APP_VERSION = "1.0.1-step2b";
 
-export const SUPABASE_URL = "https://abcd1234xyz.supabase.co";
+export const SUPABASE_URL = "https://hhnhmwfzuakyldohxnsg.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_wmuWdr8cllcThf-74YKpHw_0uV9ej30";
 
 export const TABLES = {
