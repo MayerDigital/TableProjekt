b34efd41e9081a6e591001714b483cdc061a5f57
MayerDigital <christian.mayer@mayer-bauzeichnungen.de>
2026-04-03 13:16:02 +0200

    Update screen.js

--- js/screen.js
+++ js/screen.js
@@ -7,7 +7,7 @@ import { client } from "./supabase.js";
 import {
   notifyViewer,
   getRemoteStream
-} from "./webrtc.js";
+} from "./webrtc.js?v=2";
 
 // =============================
 // REALTIME
